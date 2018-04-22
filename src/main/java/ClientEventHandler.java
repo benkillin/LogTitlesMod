27da921349a6c4dabfa64c7c84b04af1909376fc
benkillin <git.ben@b1towel.com>
2018-04-22 02:06:20 -0400

    removed un-needed imports comments and methods.

--- src/main/java/ClientEventHandler.java
+++ src/main/java/ClientEventHandler.java
@@ -1,29 +1,16 @@
-import java.lang.reflect.Field;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiIngame;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
+import net.minecraftforge.fml.relauncher.ReflectionHelper;
 import net.minecraftforge.fml.relauncher.Side;
 
 
 @Mod.EventBusSubscriber(value = Side.CLIENT, modid = LogTitlesMod.MODID)
 public class ClientEventHandler {
-	static String lastTitle = "IMPOSSIBRU";
-	static String lastSubTitle = "IMPOSSIBRU";
-
-	public static Field getField(Class clazz, String fieldName) throws NoSuchFieldException {
-        try {
-            return clazz.getDeclaredField(fieldName);
-        } catch (NoSuchFieldException e) {
-            Class superClass = clazz.getSuperclass();
-            if (superClass == null) {
-                throw e;
-            } else {
-                return getField(superClass, fieldName);
-            }
-        }
-    }
+	static String lastTitle = "displayedTitle";
+	static String lastSubTitle = "displayedSubTitle";
 
     /**
 	 * Log titles on every tick
@@ -35,12 +22,11 @@ public class ClientEventHandler {
 		final Minecraft MINECRAFT = Minecraft.getMinecraft();
 
 		if (event.phase == TickEvent.Phase.END && MINECRAFT.player != null) {
-			GuiIngame gui = MINECRAFT.ingameGUI;
-
 			try {
-				String title = getField(gui.getClass(), "displayedTitle").get(gui).toString();
-				String subTitle = getField(gui.getClass(), "displayedSubTitle").get(gui).toString();
-
+				// from http://www.minecraftforge.net/forum/topic/40032-how-to-get-the-title-and-sub-title-that-is-currently-on-screen/
+				String title = (String) ReflectionHelper.findField(GuiIngame.class, "displayedTitle", "field_175201_x").get(Minecraft.getMinecraft().ingameGUI);
+				String subTitle = (String) ReflectionHelper.findField(GuiIngame.class, "displayedSubTitle", "field_175200_y").get(Minecraft.getMinecraft().ingameGUI);
+				
 				if( (!title.trim().equals("") && !title.equals(lastTitle)) || 
 					(!subTitle.trim().equals("") && !subTitle.equals(lastSubTitle))
 				) {
@@ -50,7 +36,9 @@ public class ClientEventHandler {
 					lastSubTitle = subTitle;
 				}
 			} catch(Exception e) {
-				System.out.println(e.getMessage());
+				System.out.println("Error: unable to get the title or displayed title field. \nError message: " + 
+				e.getMessage() + "\n");
+				e.printStackTrace();
 			}
 		}
 	}
--- src/main/java/LogTitlesMod.java
+++ src/main/java/LogTitlesMod.java
@@ -1,6 +1,3 @@
-
-
-import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -8,7 +5,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 @Mod(modid = LogTitlesMod.MODID, version = LogTitlesMod.VERSION)
 public class LogTitlesMod
 {
-    public static final String MODID = "LogTitlesMod";
+    public static final String MODID = "logtitlesmod";
     public static final String VERSION = "1.0";
     public static final String NAME = "The logging titles mod";
     
